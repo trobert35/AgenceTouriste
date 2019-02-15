67155fdb5913e80c6a84f4ec5f33230e0c4c441c
thomas robert <thomas.robert32@live.fr>
2019-02-15 11:12:03 +0100

    Commit message pour le projet voyage

---
+++ src/main/java/com/fr/adaming/AgenceTouriste1Application.java
@@ -0,0 +1,14 @@
+package com.fr.adaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenceTouriste1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenceTouriste1Application.class, args);
+	}
+
+}
+
