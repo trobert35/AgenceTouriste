50c66e25e55e5d3c5ade7db5d4ef6c16532c03b7
thomas robert <thomas.robert32@live.fr>
2019-02-19 11:10:01 +0100

    Fixing PrestationServiceTest

--- AgenceTouriste_service/src/test/java/com/fr/adaming/service/PrestationServiceTest.java
+++ AgenceTouriste_service/src/test/java/com/fr/adaming/service/PrestationServiceTest.java
@@ -220,7 +220,6 @@ public class PrestationServiceTest {
 	@Test
 	public void u_readByVilleDepartAndDestination() throws ParseException {
 		//Creation d'une prestation valide puis lecture par Ville
-		// Test22
 		a_createValidPrestation();
 		assertNotNull(prestaService.readByVilleDepartArriveeAndDestination(
 				prestation.getVilleDepartArrivee(), prestation.getDestination()));
