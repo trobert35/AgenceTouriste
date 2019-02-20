9119593f3a29df275b7f77602a0eff94c7eb87df
Thomas SIMON <thomas.simon.28210@gmail.com>
2019-02-20 14:13:56 +0100

    JAVADOC interfaces couche WebService

--- AgenceTouriste_webservice/src/main/java/com/fr/adaming/restController/IAdminRestController.java
+++ AgenceTouriste_webservice/src/main/java/com/fr/adaming/restController/IAdminRestController.java
@@ -1,5 +1,9 @@
 package com.fr.adaming.restController;
 
+/**
+ * @author Mohamed EL AGREBI
+ *
+ */
 public interface IAdminRestController{
 
 }
--- AgenceTouriste_webservice/src/main/java/com/fr/adaming/restController/IAuthRestController.java
+++ AgenceTouriste_webservice/src/main/java/com/fr/adaming/restController/IAuthRestController.java
@@ -1,5 +1,9 @@
 package com.fr.adaming.restController;
 
+/**
+ * @author Mohamed EL AGREBI
+ *		   Thomas S
+ */
 public interface IAuthRestController {
 
 }
