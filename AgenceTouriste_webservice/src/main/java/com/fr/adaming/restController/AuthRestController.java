050637aba5e89756b75cb2afa908f841cc2a509d
Thomas SIMON <thomas.simon.28210@gmail.com>
2019-02-20 12:40:42 +0100

    Correction WebControllers partie 2

--- AgenceTouriste_webservice/src/main/java/com/fr/adaming/restController/AuthRestController.java
+++ AgenceTouriste_webservice/src/main/java/com/fr/adaming/restController/AuthRestController.java
@@ -13,31 +13,29 @@ import com.fr.adaming.dto.RegisterDTO;
 import com.fr.adaming.entity.User;
 import com.fr.adaming.service.IUserService;
 
-
-
 @RestController
-@RequestMapping(path="auth/")
-public class AuthRestController implements IAuthRestController{
+@RequestMapping(path = "auth/")
+public class AuthRestController implements IAuthRestController {
 
 	@Autowired
 	private IUserService<User> userService;
-	
-	@RequestMapping(path="login", method=RequestMethod.POST)
+
+	@RequestMapping(path = "login", method = RequestMethod.POST)
 	public String loginRest(@Valid @RequestBody LoginDTO logDto) {
 		User user = new User();
 		user.setEmail(logDto.getEmail());
 		user.setPwd(logDto.getPwd());
 		user = userService.login(user.getEmail(), user.getPwd());
-		return "Bonjour " + user.getPrenom() +  user.getNom() + " , vous etes bien connecte";
+		return "Bonjour " + user.getPrenom() + " " + user.getNom() + ", vous etes bien connecte";
 	}
-	
-	@RequestMapping(path="register", method=RequestMethod.POST)
+
+	@RequestMapping(path = "register", method = RequestMethod.POST)
 	public String register(@Valid @RequestBody RegisterDTO regDto) {
 		User user = new User(regDto.getNom(), regDto.getPrenom(), regDto.getEmail(), regDto.getPwd());
 		User user1 = userService.register(user);
-		return user1.getPrenom() + " " + user1.getNom() + " a correctement ete enregistre";
+		return " " + user1.getPrenom() + " " + user1.getNom() + " a ete correctement enregistre(e) ";
 	}
-	
+
 //	@RequestMapping(path="disconnect")
 //	public String disconnect() {
 //		userService.;
