ab001687b6b9034c43d0874931feb7db8c304d62
thomas robert <thomas.robert32@live.fr>
2019-02-21 15:12:46 +0100

    Rajout de l'extension de la classe Main pour acceder a l'API via Tomcat

--- AgenceTouriste_webservice/src/main/java/com/fr/adaming/AgenceVoyageApplication.java
+++ AgenceTouriste_webservice/src/main/java/com/fr/adaming/AgenceVoyageApplication.java
@@ -2,6 +2,7 @@ package com.fr.adaming;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -10,14 +11,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  *
  */
 @SpringBootApplication
-@EnableSwagger2
-public class AgenceVoyageApplication {
+public class AgenceVoyageApplication extends SpringBootServletInitializer{
 
 	/**
 	 * @param args arguments
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(AgenceVoyageApplication.class, args);
-
+			
+		System.out.println("********************************Bonjour********************************");
 	}
 }
